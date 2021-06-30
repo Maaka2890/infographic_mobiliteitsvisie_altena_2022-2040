893f36799b24e6845262a04e160ec02029dbf745
Maaka2890 <kkj_15@hotmail.com>
2021-06-30 17:51:14 +0200

    add pop up
    when clicking of svg a pop up with more info pops up. click outside the box to close, NOT ALL projects are filled in yet (only 8)

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+
 function clickFunction(projectnr) {
+    var animationspeed=800,
+        modal=$('#popupContainer'),
+        modalBG=$('#popupOverlay');
+    
+    //open popup
+    modal.css({
+      'top':'-400px',
+      'opacity':0,
+      'display':'block'
+    });
+    modalBG.fadeIn(animationspeed/2);
+    modal.delay(animationspeed/2).animate({
+          'top':'30px',
+          'opacity':1
+      }, animationspeed);
+ 
+    //close popup
+    $('#popupOverlay').click(popupClose);
+    
+    $('body').keyup(function(e){
+      if(e.which===27){popupClose();}
+    });
+    
+    $('#popupClose').click(popupClose);
+    
+    //close popup function
+    function popupClose(){
+      modalBG.delay(animationspeed).fadeOut(animationspeed);
+      modal.animate({
+        'top':'-400px',
+        'opacity':0
+      }, animationspeed/2, function(){
+        modal.css({
+          'opacity':1,
+          'display':'none'
+        });
+      });
+    };
     switch (projectnr) { // execute 
         case 1:
-            alert("this is project number 1");
+            document.getElementById("popupMsgheader").innerHTML = "1. Kernen en woongebieden verkeersveilig inrichten met focus op voetganger en fietser";
+            document.getElementById("popupMsgsubheader").innerHTML = "Door een direct fietspad te realiseren vanaf De Tol naar de Merwedebrug, wordt er een ontbrekende schakel opgevuld langs de A27 en ontstaat er een regionale (snel)fietsverbinding vanuit Gorinchem naar Oosterhout en Breda. Hiermee wordt de gemeente Altena ook direct aangesloten op het snelfietsnetwerk van Brabant en vormt het in potentie de schakel tussen de snelfietsnetwerken van de provincies Noord-Brabant en Zuid-Holland. De gemeente Altena wil dit fietspad samen met de provincie Noord-Brabant en de Regio West-Brabant gaan verkennen en realiseren.";
             break;
         case 2:
             alert("this is project number 2");
@@ -21,8 +61,9 @@ function clickFunction(projectnr) {
         case 7:
             alert("this is project number 7");
             break;
-        case 8:
-            alert("this is project number 8");
+        case 8:    
+      document.getElementById("popupMsgheader").innerHTML = "8. Verkennen en realiseren regionale fietsverbinding vanaf De Tol naar de Merwedebrug";
+      document.getElementById("popupMsgsubheader").innerHTML = "Door een direct fietspad te realiseren vanaf De Tol naar de Merwedebrug, wordt er een ontbrekende schakel opgevuld langs de A27 en ontstaat er een regionale (snel)fietsverbinding vanuit Gorinchem naar Oosterhout en Breda. Hiermee wordt de gemeente Altena ook direct aangesloten op het snelfietsnetwerk van Brabant en vormt het in potentie de schakel tussen de snelfietsnetwerken van de provincies Noord-Brabant en Zuid-Holland. De gemeente Altena wil dit fietspad samen met de provincie Noord-Brabant en de Regio West-Brabant gaan verkennen en realiseren.";
             break;
         case 9:
             alert("this is project number 9");
@@ -70,4 +111,22 @@ function clickFunction(projectnr) {
         default:
             alert("no info yet");//in svg .clickbaar onclick="clickFunction()" 
     };
+
+    // [ ] - Slect filte to load
+    // [ ] - load data from json on server
+    // [ ] - get values from loaded json
+    // [ ] - Chequ which values to select based on selection
+    // [ ] - format values
+    // [ ] - set values
+
+    var values = {
+        prtitle: "Porjectnaam",
+        prtext: "body text"
+    }
+
+    setDiagramValues();
+    function setDiagramValues() {
+        $("#selectionPannel > div.text > div > h1").text(values.prtitle); // project tietel
+        $("#selectionPannel > div.text > div > p:nth-child(3)").text(values.prtext); // sub-titel
+    }
 };
