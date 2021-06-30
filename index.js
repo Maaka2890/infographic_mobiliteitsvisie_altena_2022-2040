48ee99322b077c557521efc41dce200440a88c63
Maaka2890 <kkj_15@hotmail.com>
2021-06-30 14:08:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+function clickFunction(projectnr) {
+    switch (projectnr) { // execute 
+        case 1:
+            alert("this is project number 1");
+            break;
+        case 2:
+            alert("this is project number 2");
+            break;
+        case 3:
+            alert("this is project number 3");
+            break;
+        case 4:
+            alert("this is project number 4");
+            break;       
+        case 5:
+            alert("this is project number 5");
+            break;
+        case 6:
+            alert("this is project number 6");
+            break;
+        case 7:
+            alert("this is project number 7");
+            break;
+        case 8:
+            alert("this is project number 8");
+            break;
+        case 9:
+            alert("this is project number 9");
+            break;
+        case 10:
+            alert("this is project number 10");
+            break;
+        case 11:
+            alert("this is project number 11");
+            break;
+        case 12:
+            alert("this is project number 12");
+            break;
+        case 13:
+            alert("this is project number 13");
+            break;
+        case 14:
+            alert("this is project number 14");
+            break;
+        case 15:
+            alert("this is project number 15");
+            break;
+        case 16:
+            alert("this is project number 16");
+            break;
+        case 17:
+            alert("this is project number 17");
+            break;
+        case 18:
+            alert("this is project number 18");
+            break;
+        case 19:
+            alert("this is project number 19");
+            break;
+        case 20:
+            alert("this is project number 20");
+            break;
+        case 21:
+            alert("this is project number 21");
+            break;
+        case 22:
+            alert("this is project number 22");
+            break;
+
+        default:
+            alert("no info yet");//in svg .clickbaar onclick="clickFunction()" 
+    };
+};
